e2aeb88c84d898487de63a7d12b0954c604dd03f
sangam <sangam.shrestha1997@gmail.com>
2025-03-16 19:58:07 -0400

    Web app with postgres

---
+++ src/main/java/org/sangam/studentapp/StudentController.java
@@ -0,0 +1,20 @@
+package org.sangam.studentapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    StudentRepo studentRepo;
+
+    @RequestMapping("/getStudents")
+    public List<Student> getStudents() {
+        return studentRepo.findAll();
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO student(student_name,student_age) VALUES ('Bob',20);
+INSERT INTO student(student_name,student_age)  VALUES ('Charlie',15);
+INSERT INTO student(student_name,student_age)  VALUES ('Harry',20);
+INSERT INTO student(student_name,student_age)  VALUES ('Marie',15);
